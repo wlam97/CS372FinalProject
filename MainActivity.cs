51b1ebd1a350b999881c09baaf402f4b75e64183
wlam97 <67983229+wlam97@users.noreply.github.com>
2020-12-08 17:26:45 -0600

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,118 @@
+﻿/*
+ 
+
+CS372 - Tanzina Akter
+
+Decemeber 2nd, 2020
+ 
+Title:
+Pepe Clicker
+
+
+Tony Trinh       200367523
+Prajna Chakma    200361931
+Wilson Lam       200367497
+ 
+ */
+
+
+using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using System.Drawing;
+using Android.Content;
+using Android.Media;
+
+namespace CONSTRAINTLAYOUTTEST
+{
+    [Activity(Label = "PepeClicker", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        int clicks = 0;
+        ImageButton pepeimagebutton;
+        TextView pepetextview;
+        Button storebutton;
+        string color;
+        MediaPlayer shanty;
+        protected override void OnCreate(Bundle savedInstanceState) //function called oncreation
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            shanty = MediaPlayer.Create(this, Resource.Raw.shanty2); //music will play
+            shanty.Start();
+            clicks = GetClicks();                                   // receiving clicks if values are changed within store
+            color = GetColor();                                     //this will recognize if a color is bought in the store and update it
+            pepeimagebutton = FindViewById<ImageButton>(Resource.Id.pepeimagebutton);
+            pepetextview = FindViewById<TextView>(Resource.Id.pepetextview);
+            pepeimagebutton.Click += pepeimagebutton_click;
+            storebutton = FindViewById<Button>(Resource.Id.storebutton);
+            storebutton.Click += storebutton_click;
+
+
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent? data)
+        {
+            if (requestCode == 1)           //receives data from store and passes it back to the main game
+            {
+                if (resultCode == Result.Ok)
+                {
+                    clicks = data.GetIntExtra("clicksback", clicks);
+                    color = data.GetStringExtra("color");
+                    pepetextview.Text = clicks.ToString() + " PEPES";       //reupdating currency to match game to store
+
+                    if (color == "orange")
+                    {
+                        pepeimagebutton.SetImageResource(Resource.Drawable.orange);         //if "color" is bought, it is set to that color
+                    }
+                    if (color == "yellow")
+                    {
+                        pepeimagebutton.SetImageResource(Resource.Drawable.yellow);
+                    }
+                    if (color == "green")
+                    {
+                        pepeimagebutton.SetImageResource(Resource.Drawable.green);
+                    }
+                    if (color == "blue")
+                    {
+                        pepeimagebutton.SetImageResource(Resource.Drawable.blue);
+                    }
+                    if (color == "violet")
+                    {
+                        pepeimagebutton.SetImageResource(Resource.Drawable.rainbow);
+                    }
+                }
+            }
+        }
+
+        int GetClicks()
+        {
+            return Intent.GetIntExtra("clicksback", clicks);        //functions to receive clicks
+        }
+
+        void storebutton_click(object sender, System.EventArgs e)
+        {
+            Intent nextActivity = new Intent(this, typeof(StoreActivity));          //function to send clicks/currency and start store activity
+            nextActivity.PutExtra("clicks", clicks);
+            StartActivityForResult(nextActivity, 1);
+        }
+
+        string GetColor()
+        {
+            return Intent.GetStringExtra("color" ?? "Not received"); //receives color
+        }
+
+        void pepeimagebutton_click(object sender, System.EventArgs e) //increments on click
+        {
+            clicks++;
+            pepetextview.Text = clicks.ToString() + " PEPES";
+
+        }
+    }
+
+}
---
+++ StoreActivity.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using System.Drawing;
+using Android.Content;
+using Android.Media;
+
+namespace CONSTRAINTLAYOUTTEST
+{
+    [Activity(Label = "PepeClicker Store")]
+    public class StoreActivity : AppCompatActivity
+    {
+        int clicks = 0;
+        int clickstostring = 0;
+        ImageButton orange;
+        ImageButton yellow;
+        ImageButton green;
+        ImageButton blue;
+        ImageButton violet;
+        TextView currencyclick;
+        string color;
+        MediaPlayer SellPepe;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.content_main);
+            Button backtopepe = FindViewById<Button>(Resource.Id.backtopepe);           
+            SellPepe = MediaPlayer.Create(this, Resource.Raw.SellCoins);            //variable to play music on successful purchase
+            clicks = GetClicks();                                                   //getting clicks from main
+            clickstostring = clicks;                                                //duping variables to get our clicks to a string
+            currencyclick = FindViewById<TextView>(Resource.Id.currencyclick);      //updating currency in shop to show as string
+            currencyclick.Text = clickstostring.ToString() + " PEPES";
+
+            orange = FindViewById<ImageButton>(Resource.Id.storebutton1); //one of the store functions for purchasing a color
+            orange.Click += orangebutton_click;
+
+            yellow = FindViewById<ImageButton>(Resource.Id.storebutton2);
+            yellow.Click += yellowbutton_click;
+
+            green = FindViewById<ImageButton>(Resource.Id.storebutton3);
+            green.Click += greenbutton_click;
+
+            blue = FindViewById<ImageButton>(Resource.Id.storebutton4);
+            blue.Click += bluebutton_click;
+
+            violet = FindViewById<ImageButton>(Resource.Id.storebutton5);
+            violet.Click += violetbutton_click;
+
+
+            backtopepe.Click += backtopepe_click;                           //function to go back to the game from the store
+
+
+        }
+
+        void orangebutton_click(object sender, System.EventArgs e)          //if orange is clicked, pepe will be orange and currency will be subtracted + music play
+        {
+            if (clicks >= 25)
+            {
+                clicks = clicks - 25;
+                clickstostring = clicks;
+                currencyclick.Text = clickstostring.ToString() + " PEPES";
+                color = "orange";
+                SellPepe.Start();
+            }
+        }
+
+        void yellowbutton_click(object sender, System.EventArgs e)
+        {
+            if (clicks >= 25)
+            {
+                clicks = clicks - 25;
+                clickstostring = clicks;
+                currencyclick.Text = clickstostring.ToString() + " PEPES";
+                color = "yellow";
+                SellPepe.Start();
+            }
+        }
+
+        void greenbutton_click(object sender, System.EventArgs e)
+        {
+            if (clicks >= 25)
+            {
+                clicks = clicks - 25;
+                clickstostring = clicks;
+                currencyclick.Text = clickstostring.ToString() + " PEPES";
+                color = "green";
+                SellPepe.Start();
+            }
+        }
+
+        void bluebutton_click(object sender, System.EventArgs e)
+        {
+            if (clicks >= 25)
+            {
+                clicks = clicks - 25;
+                clickstostring = clicks;
+                currencyclick.Text = clickstostring.ToString() + " PEPES";
+                color = "blue";
+                SellPepe.Start();
+            }
+        }
+
+        void violetbutton_click(object sender, System.EventArgs e)
+        {
+            if (clicks >= 25)
+            {
+                clicks = clicks - 25;
+                clickstostring = clicks;
+                currencyclick.Text = clickstostring.ToString() + " PEPES";
+                color = "violet";
+                SellPepe.Start();
+            }
+        }
+
+
+
+        int GetClicks()
+        {
+            return Intent.GetIntExtra("clicks", clicks);                    //getting clicks from main game
+        }
+
+        void backtopepe_click(object sender, System.EventArgs e)                //sending color and clicks back to main game and finishes the activity
+        {
+            Intent back = new Intent();
+            back.PutExtra("clicksback", clicks);
+            back.PutExtra("color", color);
+            SetResult(Result.Ok, back);
+            Finish();
+        }
+    }
+}
